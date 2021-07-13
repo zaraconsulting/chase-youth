70e30c3ce4361c8bed0692ff7febcb1eaac75653
derekcodingtemple <derekh@codingtemple.com>
2021-07-12 20:06:09 -0500

    update

--- chase_youth/settings.py
+++ chase_youth/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', 'chase-youth.herokuapp.com']
 
 
 # Application definition
