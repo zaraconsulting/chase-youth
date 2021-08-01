8bf82a7397f65404f93952d6042afc88d6bb0e5b
zaraconsulting <derek@zaraconsulting.org>
2021-08-01 18:45:12 -0500

    updated Facebook link

--- chase_youth/context_processor.py
+++ chase_youth/context_processor.py
@@ -5,6 +5,7 @@ def export_context(request):
     data = {
         'COMPANY_NAME': env('COMPANY_NAME'),
         'EVENTS': Event.objects.order_by('-created_on').all(),
-        'ADMIN_EMAIL_TO': env('ADMIN_EMAIL_TO')
+        'ADMIN_EMAIL_TO': env('ADMIN_EMAIL_TO'),
+        'FACEBOOK_URL': env('FACEBOOK_URL')
     }
     return data
