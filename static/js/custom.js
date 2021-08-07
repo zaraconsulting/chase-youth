2d7da868f33f4475a504fb03b9d2da3a00f4d011
zaraconsulting <derek@zaraconsulting.org>
2021-08-07 13:25:58 -0500

    click snapchat link to copy to clipboard

---
+++ static/js/custom.js
@@ -0,0 +1,25 @@
+$('#snapchat').on('mouseover', () => {
+    $('#snapchat-copy-link').text('Click to copy');
+});
+$('#snapchat').on('mouseout', () => {
+    $('#snapchat-copy-link').text('$CHASEYOUTH');
+});
+
+function copyToClipboard()
+{
+    const el = document.createElement('textarea');
+    el.value = '$CHASEYOUTH';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+
+    $('#snapchat-copy-link').text('Copied!')
+    setTimeout(() =>
+    {
+        $('#snapchat-copy-link').text('$CHASEYOUTH');
+    }, 2000);
+
+
+}
+document.querySelector('#snapchat-copy-link').addEventListener('click', copyToClipboard)
