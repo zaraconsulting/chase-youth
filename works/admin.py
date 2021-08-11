e6de73b91f33a211069b1dd72bcb256163a41485
zaraconsulting <derek@zaraconsulting.org>
2021-08-10 19:01:02 -0500

    added image field to works

--- works/admin.py
+++ works/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Work, TitleFontColor, TitleFontSize, ExcerptFontColor, ExcerptFontSize
 
 class WorksAdmin(admin.ModelAdmin):
-    fields = ('title', 'title_font_size', 'title_font_color', 'excerpt', 'excerpt_font_size', 'excerpt_font_color')
+    fields = ('title', 'image', 'title_font_size', 'title_font_color', 'excerpt', 'excerpt_font_size', 'excerpt_font_color')
 
 # Register your models here.
 admin.site.register(Work, WorksAdmin)
